69620d6a034267e02084b05a4ca0baf495d28e52
Anthony Rawlings <rawlings808@hotmail.com>
2021-11-28 19:43:57 -0600

    added alert for empty values

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+
+window.addEventListener("load", function () {
+    // const pilotName = document.getElementById("pilotName");
+    // const copilotName = document.getElementById("copilotName");
+    // const fuelLevel = document.getElementById("fuelLevel");
+    // const cargoMass = document.getElementById("cargoMass");
+    // const button = document.getElementById("formSubmit");
+    
+
+    
+    // console.log(form.pilotName.value);
+    // console.log(form.copilotName);
+
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", function () {
+        console.log("Submitting form");
+        validateInput(form);
+        
+    });
+
+
+
+
+    
+    // let listedPlanets;
+    // // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    // let listedPlanetsResponse;
+    // listedPlanetsResponse.then(function (result) {
+    //     listedPlanets = result;
+    //     console.log(listedPlanets);
+    // }).then(function () {
+    //     console.log(listedPlanets);
+    //     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    // })
+
 });
+
+function newFunction() {
+    module.import; { validateInput; } from; "./scriptHelper.js";
+}
